70d40e176485ebf2bfef732575730b96209831b4
farbod-kh <farbod.khosrozadeh@gmail.com>
2023-03-03 11:24:54 +0330

    1

--- WindowsFormsApp16/Form1.cs
+++ WindowsFormsApp16/Form1.cs
@@ -16,5 +16,10 @@ namespace WindowsFormsApp16
         {
             InitializeComponent();
         }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Test");
+        }
     }
 }
